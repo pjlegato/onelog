50192a031dccf51536ca76b5aa56a6947e3c09a6
pjlegato@gmail.com <pjlegato@gmail.com>
2013-01-09 13:07:28 -0800

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onelog "0.3.0-SNAPSHOT"
+(defproject onelog "0.3.0"
   :description "Batteries-included logging for Clojure"
   :url "https://github.com.com/pjlegato/onelog"
   :license {:name "Eclipse Public License"
