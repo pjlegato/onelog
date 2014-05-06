b626f41197bee7f6bb8ad56c010b5f36811bbcb2
Paul Legato <pjlegato@builtonlogic.com>
2014-05-05 18:48:49 -0700

    Bump versions

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/tools.logging "0.2.3"]
-                 [clj-logging-config "1.9.6"]
-                 [org.clojars.pjlegato/clansi "1.3.0"]
-                 [org.clojure/clojure "1.4.0"]])
+                 [org.clojure/tools.logging "0.2.6"]
+                 [clj-logging-config "1.9.10"]
+                 [org.clojars.pjlegato/clansi "1.3.0"]])
