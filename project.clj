73e9ae88cb068db14a4d745275cfc672fd9d6b49
Paul Legato <pjlegato@gmail.com>
2015-05-22 15:36:05 -0700

    Avoid log4j version collision

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/tools.logging "0.2.6"]
-                 [clj-logging-config "1.9.10"]
+                 [clj-logging-config "1.9.10" :exclusions [log4j]]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.7"]
                  [io.aviso/pretty "0.1.12"] ;; Pretty printer / exception formatter
                  ])
+
